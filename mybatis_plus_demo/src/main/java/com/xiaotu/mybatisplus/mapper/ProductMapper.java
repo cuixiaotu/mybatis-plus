5e4ca009e10142d7cbac540893baeb349961ce58
theodore <cuixin1974@stnts.com>
2022-07-19 11:52:18 +0800

    第六章 插件完结

---
+++ mybatis_plus_demo/src/main/java/com/xiaotu/mybatisplus/mapper/ProductMapper.java
@@ -0,0 +1,8 @@
+package com.xiaotu.mybatisplus.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiaotu.mybatisplus.poji.Product;
+
+public interface ProductMapper extends BaseMapper<Product> {
+
+}
--- mybatis_plus_demo/src/test/java/com/xiaotu/mybatisplus/MyBatisPlusPluginsTest.java
+++ mybatis_plus_demo/src/test/java/com/xiaotu/mybatisplus/MyBatisPlusPluginsTest.java
@@ -1,7 +1,9 @@
 package com.xiaotu.mybatisplus;
 
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.xiaotu.mybatisplus.mapper.ProductMapper;
 import com.xiaotu.mybatisplus.mapper.UserMapper;
+import com.xiaotu.mybatisplus.poji.Product;
 import com.xiaotu.mybatisplus.poji.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +16,10 @@ public class MyBatisPlusPluginsTest {
 
     @Autowired
     UserMapper userMapper;
+
+    @Autowired
+    ProductMapper productMapper;
+
 
     @Test
     public void testPage(){
@@ -37,7 +43,7 @@ public class MyBatisPlusPluginsTest {
     public void testSelectPageVo(){
         //设置分页参数
         Page<User> page = new Page<>(1,5);
-        userMapper.selectPageVo(page,20);
+        userMapper.selectPageVo(page,10);
         //获取分页数据
         List<User> list = page.getRecords();
         list.forEach(System.out::println);
@@ -48,6 +54,55 @@ public class MyBatisPlusPluginsTest {
         System.out.println("总页数："+page.getPages());
         System.out.println("是否有上一页："+page.hasPrevious());
         System.out.println("是否有下一页："+page.hasNext());
+    }
+
+    @Test
+    public void testConcurrentUpdate() {
+        //1、小李
+        Product p1 = productMapper.selectById(1L);
+        System.out.println("小李取出的价格：" + p1.getPrice());
+        //2、小王
+        Product p2 = productMapper.selectById(1L);
+        System.out.println("小王取出的价格：" + p2.getPrice());
+        //3、小李将价格加了50元，存入了数据库
+        p1.setPrice(p1.getPrice() + 50);
+        int result1 = productMapper.updateById(p1);
+        System.out.println("小李修改结果：" + result1);
+        //4、小王将商品减了30元，存入了数据库
+        p2.setPrice(p2.getPrice() - 30);
+        int result2 = productMapper.updateById(p2);
+        System.out.println("小王修改结果：" + result2);
+        //最后的结果
+        Product p3 = productMapper.selectById(1L);
+        //价格覆盖，最后的结果：70
+        System.out.println("最后的结果：" + p3.getPrice());
+    }
+
+
+    @Test
+    public void testConcurrentVersionUpdate() {
+        //小李取数据
+        Product p1 = productMapper.selectById(1L);
+        //小王取数据
+        Product p2 = productMapper.selectById(1L);
+        //小李修改 + 50
+        p1.setPrice(p1.getPrice() + 50);
+        int result1 = productMapper.updateById(p1);
+        System.out.println("小李修改的结果：" + result1);
+        //小王修改 - 30
+        p2.setPrice(p2.getPrice() - 30);
+        int result2 = productMapper.updateById(p2);
+        System.out.println("小王修改的结果：" + result2);
+        if(result2 == 0){
+        //失败重试，重新获取version并更新
+        p2 = productMapper.selectById(1L);
+        p2.setPrice(p2.getPrice() - 30);
+        result2 = productMapper.updateById(p2);
+        }
+        System.out.println("小王修改重试的结果：" + result2);
+        //老板看价格
+        Product p3 = productMapper.selectById(1L);
+        System.out.println("老板看价格：" + p3.getPrice());
     }
 
 }
